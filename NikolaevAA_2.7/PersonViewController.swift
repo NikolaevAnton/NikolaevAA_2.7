719a31fe6f10d89498058a1d019c1a21299ddf7f
AANikolaev <mjop41@gmail.com>
2021-11-10 14:51:19 +0300

    набросал эскиз

---
+++ NikolaevAA_2.7/PersonViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PersonViewController.swift
+//  NikolaevAA_2.7
+//
+//  Created by Anton Nikolaev on 10.11.2021.
+//
+
+import UIKit
+
+class PersonViewController: UIViewController {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
